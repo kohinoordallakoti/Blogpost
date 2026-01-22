baf2d6f9f0d95e03ebfb052255ff638d434fab83
kohinoordallakoti <kohinoordallakoti@gmail.com>
2026-01-22 17:29:02 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use((err,req,res,next) => {
 })
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://blogging-8r9u.onrender.com",
   methods:["GET","POST","PUT","DELETE"],
   credentials:true,
 })  );
