a7d332b37a46d76964d957a8f04877d76edd5de2
kohinoordallakoti <kohinoordallakoti@gmail.com>
2026-01-22 17:35:26 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,45 @@
-import express from 'express'
-import connectDB from './config/db.js'
-import dotenv from 'dotenv'
-import userRoutes from './routes/userRoutes.js'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import blogRoutes from './routes/blogRoutes.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import contactRoutes from './routes/contactRoutes.js'
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import blogRoutes from "./routes/blogRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
 
-dotenv.config()
+dotenv.config();
 connectDB();
+
 const app = express();
+
+/* ✅ CORS FIRST */
 app.use(cors({
-  origin:"https://blogging-8r9u.onrender.com",
-  methods:["GET","POST","PUT","DELETE"],
-  credentials:true,
-})  );
+  origin: "https://blogging-8r9u.onrender.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+app.options("*", cors({
+  origin: "https://blogging-8r9u.onrender.com",
+  credentials: true,
+}));
+
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
 
-// app.use((req,res,next) =>{
-
-//   console.log("this is  middleware")
-//   next();
-// })
-
-
-app.use((err,req,res,next) => {
-  console.log(err)
-  next();
-})
-
-app.use('/user', userRoutes)
-
-app.use('/blog', blogRoutes)
-
-app.use('/category', categoryRoutes)
-
-app.use('/contact', contactRoutes)
-
+/* Routes */
+app.use("/user", userRoutes);
+app.use("/blog", blogRoutes);
+app.use("/category", categoryRoutes);
+app.use("/contact", contactRoutes);
 app.use("/upload", express.static("upload"));
 
 app.get("/", (req, res) => {
-    res.send("Server is running")
-})
+  res.send("Server is running");
+});
 
 app.listen(5000, () => {
-    console.log("Server is running at http://localhost:5000");
-})
+  console.log("Server running on port 5000");
+});
